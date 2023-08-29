515f306055c6cb78dce0df502057e6dadfd23118
junho-yoon99 <junho.yoon@mckinleyrice.co>
2023-08-29 18:59:43 +0900

    docs : 2. prisma-mongodb

--- Readme.md
+++ Readme.md
@@ -4,6 +4,6 @@
 
 - Reference(Official Document) : https://www.prisma.io/docs/reference
 
-1. [Start Prisma with MySQL(RDB)](./docs/1.%20Quick%20Start.md)
-2. Start Prisma with MongoDB(NoSQL)
+1. [Start Prisma with MySQL(RDB)](./docs/1.%20Prisma-MySQL.md)
+2. [Start Prisma with MongoDB(NoSQL)](./docs/2.%20Prisma-Mongodb.md)
 3. Prisma Transaction and Index
---
+++ src/prisma-mongo/create.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: "email@gmail.com",
+      name: "name",
+      //
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect();
+  });
